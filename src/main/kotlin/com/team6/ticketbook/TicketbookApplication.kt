c00056ffd7703506b53878a27e4659b457a93ae3
AWKRID <minzoo202738@gmail.com>
2024-07-02 19:12:09 +0900

    :tada: create: Initiate new project

---
+++ src/main/kotlin/com/team6/ticketbook/TicketbookApplication.kt
@@ -0,0 +1,11 @@
+package com.team6.ticketbook
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TicketbookApplication
+
+fun main(args: Array<String>) {
+    runApplication<TicketbookApplication>(*args)
+}
---
+++ src/test/kotlin/com/team6/ticketbook/TicketbookApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.team6.ticketbook
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TicketbookApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
