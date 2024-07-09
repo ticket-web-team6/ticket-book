d787f6edccb3a848e92a105e216bf1ded3c07693
AWKRID <minzoo202738@gmail.com>
2024-07-09 14:37:05 +0900

    :bug: fix: Update lock function in LockService

--- src/main/kotlin/com/team6/ticketbook/domain/lock/service/LockService.kt
+++ src/main/kotlin/com/team6/ticketbook/domain/lock/service/LockService.kt
@@ -2,19 +2,17 @@ package com.team6.ticketbook.domain.lock.service
 
 import com.team6.ticketbook.domain.lock.model.LockData
 import com.team6.ticketbook.domain.lock.repository.LockRepository
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class LockService(
     private val lockRepository: LockRepository,
-    @Value("\${lock.code}") private val lockCode: String,
 
     ) {
     @Transactional
     fun lock(code: String): LockData {
-        return lockRepository.findByCode(lockCode) ?: run {
+        return lockRepository.findByCode(code) ?: run {
             LockData(
                 code = code,
                 lockNum = 0
